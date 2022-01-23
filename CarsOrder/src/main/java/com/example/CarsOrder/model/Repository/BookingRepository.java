e7a6e2169062fd245d322ea7d6b63002e241826b
Amal <92294553+amalwash@users.noreply.github.com>
2022-01-23 11:03:02 +0300

    Delete CarsOrder directory

--- CarsOrder/src/main/java/com/example/CarsOrder/model/Repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.CarsOrder.model.Repository;
-
-import com.example.CarsOrder.model.Entitiec.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository  extends JpaRepository<Booking, Integer> {
-}
--- CarsOrder/src/main/java/com/example/CarsOrder/model/Repository/OwnerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CarsOrder.model.Repository;
-import com.example.CarsOrder.model.Entitiec.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerRepository  extends JpaRepository<Owner, Integer> {
-}
